09f4a9cac78a770e8b42de4fc4588a1eaf17ef8f
Henry Khosasih <henrylaurentkhosasih@gmail.com>
2022-04-04 18:49:31 +1000

    Add ChatEngine component with a test user

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { ChatEngine } from "react-chat-engine";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <ChatEngine
+        height='100vh'
+        userName='henry'
+        userSecret='123456'
+        projectID='0452794c-a863-4e55-a613-dc1355e34a25'
+		/>
     </div>
   );
 }
